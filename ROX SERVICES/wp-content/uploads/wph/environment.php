7933152806b7dbe16949015a727e85581687788e
Paulo Henrique de Souza Silva <phsouzasilva27@gmail.com>
2021-10-25 16:48:25 -0300

    versão 1.0

---
+++ ROX SERVICES/wp-content/uploads/wph/environment.php
@@ -0,0 +1,2 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+$environment_variable = '{"theme":{"folder_name":"oceanwp","mapped_name":""},"allowed_paths":["\/home\/u671890389\/domains\/roxservices.net\/public_html\/wp-content\/themes"],"cache_path":"\/home\/u671890389\/domains\/roxservices.net\/public_html\/wp-content\/cache\/wph\/","wordpress_directory":"","site_relative_path":"\/"}'; ?>
---
+++ ROX SERVICES/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:" j}9PVF?&K]SB(H$J+qoKZTBrJ!r4|bMAy*Hk>PupX7sLfmF-Ert0*D0;$I|g$p:";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2975;}
